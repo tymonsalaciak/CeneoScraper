0a32d1f467f1f90837d9ee72e6b8e12d025a3224
tymonsalaciak <tymo4444@wp.pl>
2023-04-05 16:35:33 +0200

    2023.04.05

--- scraper.py
+++ scraper.py
@@ -6,6 +6,30 @@ from bs4 import BeautifulSoup
 product_code = 96693065
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews_scroll"
 response = requests.get(url)
-peage_dom = BeautifulSoup(response.text, "html.parser") # dwa argumenty
+page_dom = BeautifulSoup(response.text, "html.parser") # dwa argumenty
+opinions = page_dom.select("div.js_product-review") # . odpowiada za class
+all_opinions = []
+for opinion in opinions:
+    single_opion = {
+        "oinion_id": opinion["data-entry-id"],
+        "author": opinion.select_one("span.user-post__author-name").text.strip(),
+        "recommendation": opinion.select_one("span.user-post__author-recomendation > em").text.strip(),
+        "rating": opinion.select_one("user-post__score-count").text.strip(),
+        "verafied": opinion.select_one("div.review-pz").text.strip(),
+        "post_date": opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"].strip(),
+        "purchase_date": opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"].strip(),
+        "vote_up": opinion.select_one("buton.vote-yes")["data-total-vote"].strip(),
+        "vote_down": opinion.select_one("buton.vote-no")["data-total-vote"].strip(),
+        "content": opinion.select_one("div.user-post__text").text.strip(),
+        "cons": [cons.text.strip() for cons in  opinion.select_one("div.review-feature__title--negatives~div.review-feature__item")],
+        "pros": [pros.text.strip() for pros in  opinion.select_one("div.review-feature__title--positives~div.review-feature__item")],
 
-print(peage_dom.prettify()) # .text .status_code response.text
+    }
+    print(type(opinions))
+#print(page_dom.prettify()) # .text .status_code response.text
+#[cons.text.strip() for cons in  opinion.select_one("div.review-feature__title--negatives~div.review-feature__item") ]
+all_opinions.append(single_opion)
+print(all_opinions)
+
+
+
