6d2be8de6b95b1e5b589fb282244ef97808f89dc
tymonsalaciak <tymo4444@wp.pl>
2023-04-12 16:30:53 +0200

    12.04.23 Tymons

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # CeneoScraper
 pp2
+
+# Selektory ceneo.pl
+| składowa | nazwa | sektor |
+| --- | --- | --- |
+| identyfikator opinii | opinion\_id | [data-entry-id] |
+| autora | author | span.user.post\_\_author-name |
+| rekomendację | recommendation | span.user-post\_\_author-recomendation\>em |
+| liczbę gwiazdek | rating | user-post\_\_score-count |
+| czy opinia jest potwierdzona zakupem | verified | div.review-pz |
+| data wystawienia opinii | post\_data | span.user-post\_\_published\>time:nth-child(1)[datatime] |
+| data zakupu produktu | purchase\_date | span.user-post\_\_published\>time:nth-child(2)[[datatime] |
+| ile osób uznało opinię za przydatną | vote\_up | buton.vote-yes[data-total-vote]buton.vote-yes\>span |
+| ile osób uznało opinię za nieprzydatną | vote\_down | buton.vote-no[data-total-vote]buton.vote-no\>span |
+| treść opinii | content | div.user-post\_\_text |
+| listę wad | pons | div.review-feature\_\_title—negatives~div.review-feature\_\_item |
+| listę zalet | pros | div.review-feature\_\_title—positives~div.review-feature\_\_item |
+
+
+
+## wykorzystane biblioteki
+-BeautifulSoup4
+-Requests
--- scraper.py
+++ scraper.py
@@ -1,6 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
+def extract_tag(ancestor, selector=None, attribiute=None, return_list=False): # none może przyjąc rózne zmienne / false w tym przypadku jest zabiegiem logicznym
+    try:
+        if return_list:
+            return [tag.text.strip() for tag in  ancestor.select(selector)]
+        if not selector and attribiute:
+            return ancestor[attribiute]
+        if attribiute:
+            return ancestor.select_one(selector)[attribiute].strip()
+        return ancestor.select_one(selector).text.strip()
+    except (AttributeError, TypeError):
+        return None
 
 #product_code = input("Podaj kod produktu: ")
 product_code = 96693065
@@ -11,25 +23,30 @@ opinions = page_dom.select("div.js_product-review") # . odpowiada za class
 all_opinions = []
 for opinion in opinions:
     single_opion = {
-        "oinion_id": opinion["data-entry-id"],
-        "author": opinion.select_one("span.user-post__author-name").text.strip(),
-        "recommendation": opinion.select_one("span.user-post__author-recomendation > em").text.strip(),
-        "rating": opinion.select_one("user-post__score-count").text.strip(),
-        "verafied": opinion.select_one("div.review-pz").text.strip(),
-        "post_date": opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"].strip(),
-        "purchase_date": opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"].strip(),
-        "vote_up": opinion.select_one("buton.vote-yes")["data-total-vote"].strip(),
-        "vote_down": opinion.select_one("buton.vote-no")["data-total-vote"].strip(),
-        "content": opinion.select_one("div.user-post__text").text.strip(),
-        "cons": [cons.text.strip() for cons in  opinion.select_one("div.review-feature__title--negatives~div.review-feature__item")],
-        "pros": [pros.text.strip() for pros in  opinion.select_one("div.review-feature__title--positives~div.review-feature__item")],
+        "oinion_id": extract_tag(opinion,None ,"data-entry-id"),
+        "author": extract_tag(opinion,"span.user-post__author-name"),
+        "recommendation": extract_tag(opinion,"span.user-post__author-recomendation > em"),
+        "rating": extract_tag(opinion,".user-post__score-count"),
+        "verafied": extract_tag(opinion,"div.review-pz"),
+        "post_date":  extract_tag(opinion,"span.user-post__published > time:nth-child(1)","datetime"),
+        "purchase_date":  extract_tag(opinion,"span.user-post__published > time:nth-child(2)","datetime"),
+        "vote_up":  extract_tag(opinion,"button.vote-yes","data-total-vote"),
+        "vote_down":  extract_tag(opinion,"button.vote-no","data-total-vote"),
+        "content": extract_tag(opinion,"div.user-post__text"),
+        "cons": extract_tag(opinion,"div.review-feature__title--negatives~div.review-feature__item", None, True),
+        "pros": extract_tag(opinion,"div.review-feature__title--positives~div.review-feature__item", None, True),
 
     }
-    print(type(opinions))
+    all_opinions.append(single_opion)
+with open(f"./opinions/{product_code}.json", "w", encoding="UTF-8") as  jf:
+    json.dump(all_opinions, jf, indent=4 , ensure_ascii=False)
+
+
+
+    #print(type(opinions))
 #print(page_dom.prettify()) # .text .status_code response.text
 #[cons.text.strip() for cons in  opinion.select_one("div.review-feature__title--negatives~div.review-feature__item") ]
-all_opinions.append(single_opion)
-print(all_opinions)
+
 
 
 
